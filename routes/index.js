23e519b91a60039502e44748dca17a6e54a69eb7
Muhammad Faseeh <99120691+MuhammadFASEEH2002@users.noreply.github.com>
2024-02-02 16:09:59 +0500

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ const router = express.Router();
 
 import * as authController from '../controllers/authController.js'
 
+async (req, res) => {
 
+}
+router.post("/",async (req, res) => {
+console.log("hello")
+})
 router.post('/register', authController.register)
 router.post('/login', authController.login)
 
