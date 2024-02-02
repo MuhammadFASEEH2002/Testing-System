577ed1a1feb66db032337818eef732d0ce05365f
Muhammad Faseeh <99120691+MuhammadFASEEH2002@users.noreply.github.com>
2024-02-03 01:10:55 +0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,28 +1,9 @@
-// import express from "express";
-// import indexRoute from "./routes/index.js";
-// const app = express();
-// const PORT = process.env.PORT || 5000;
-
-// app.use(express.json());
-// app.use("/", indexRoute);
-
-// app.listen(PORT, () =>
-//   console.log(`Server is listening on http://localhost:${PORT}/`)
-// );
-
-
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const env = require('dotenv').config();
 const cors = require("cors");
-
-// const verifyToken = require('./middleware/verifyToken')
-// const AuthRouer = require("./controllers/Auth");
 const IndexRouter = require('./routes/index.js')
-// const AdminRouter = require('./routes/adminRoutes')
-// const InvesteeRouter = require('./routes/investeeRoutes')
-// const InvestorRouter = require('./routes/investorRoutes')
 
 
 
@@ -38,7 +19,6 @@ mongoose
   .then((res) => console.log("MongoDB connected"))
   .catch((err) => console.log("MongoDb Connection Failed", err));
 app.use(express.json());
-// app.use(express.static('upload'));
 app.use(express.urlencoded({ extended: true }));
 app.use("/api", IndexRouter);
 
