7c45e63f2a1caf6a067ce9cf92aa362906243dcb
Muhammad Faseeh <99120691+MuhammadFASEEH2002@users.noreply.github.com>
2024-02-02 22:28:09 +0500

    updates

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router()
 const authController  = require('../controllers/authController.js')
-
+router.get('/msg', async (req, res) => {console.log("hello")})
 router.post('/register', authController.register)
 router.post('/login', authController.login)
 
