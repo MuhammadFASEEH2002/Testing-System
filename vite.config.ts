bddbd70068c12bea54518d9dbe0a394d195eb2f6
Muhammad Faseeh <99120691+MuhammadFASEEH2002@users.noreply.github.com>
2024-02-02 15:35:39 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   server:{
     proxy:{
       '/api':{
-        target:'https://testify-ten.vercel.app:5000',
+        target:'https://testify-ten.vercel.app',
         changeOrigin: true, 
         secure: false, 
         rewrite: (path) => path.replace(/^\/api/, ''),
