b63b4e8a4bae5e05d461617cb2ffd615c0d76d9c
Muhammad Faseeh <99120691+MuhammadFASEEH2002@users.noreply.github.com>
2024-02-03 16:38:34 +0500

    Update api.ts

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -3,7 +3,7 @@ import axios from 'axios'
 const api = axios.create({
    //@ts-ignore
   baseURL : `${process.env.AXIOS_LINK}`,
-  timeout : 15000,
+  timeout : 30000,
   withCredentials : true
 })
 
