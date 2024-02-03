1f2328a45c77201f3abcb6ed73f52155ee603def
Muhammad Faseeh <99120691+MuhammadFASEEH2002@users.noreply.github.com>
2024-02-03 16:45:34 +0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const IndexRouter = require('./routes/index.js')
 
 app.use(
   cors({
-    origin: ["http://localhost:5173","https://testify-client.vercel.app"],
+    origin: "*",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
