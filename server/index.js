54d6241ffc45ad77c6d083f59365448fcc6d5242
Muhammad Faseeh <99120691+MuhammadFASEEH2002@users.noreply.github.com>
2024-02-03 16:49:02 +0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -7,13 +7,14 @@ const IndexRouter = require('./routes/index.js')
 
 
 
-app.use(
-  cors({
-    origin: "*",
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true,
-  })
-);
+// app.use(
+//   cors({
+//     origin: "*",
+//     methods: ["GET", "POST", "PUT", "DELETE"],
+//     credentials: true,
+//   })
+// );
+app.use(cors())
 mongoose
   .connect(`${process.env.MONGO_LINK}`)
   .then((res) => {console.log("MongoDB connected"); console.log(process.env.CORS_ORIGIN)})
